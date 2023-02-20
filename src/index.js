44845edb099cf9c48e264053c7f4da8377b840ed
MariaAbdullah97 <meroo3066@gmail.com>
2023-02-20 20:41:19 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
+    const form =document.querySelector("form");
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault()
+        const input=document.querySelector('input')
+        fetch(` http://localhost:3000/movies/${input.value}`)
+        .then((res)=>res.json())
+        .then((data)=>renderdata(data))
+    })
   
+}
+
+function renderdata(data){
+    const header=document.querySelector('h4')
+    const prag=document.querySelector('p')
+    header.innerHTML=`${data.title}`
+    prag.innerHTML=`${data.summary}`
 }
 
 document.addEventListener('DOMContentLoaded', init);
